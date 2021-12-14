8a2583aab99dd3faf4b724487118ef642625c784
HafizHuzaifa2 <94917958+HafizHuzaifa2@users.noreply.github.com>
2021-12-14 12:19:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,192 @@
+// (Chapter Number 1)
+
+// alert ("ALERT");
+// alert ("Erorr! Please enter a valid password");
+// alert ("Welcome to JS land....","Happy Coding!");
+// alert ("Welcom to JS land");
+// alert ("Happy coding!","Prevent this page from an additional dialogs.");
+// alert ("Hello! I can run JS through my computer browser.");
+
+
+// (Chapter Number 2)
+
+// var UserName = "Hafiz Huzaifa"
+// alert(UserName,"UserName")
+// var UserFatherName = "M.Ali"
+// alert(UserFatherName,"UserFatherName")
+// var Email = "huzaifaali0343@gmail.com"
+// alert(Email,"Enter Email")
+// var Age = 18
+// alert(Age)
+
+// (Chapter Number 3)
+
+// var UserBirth = 2001
+// alert(UserBirth)
+// var UserAge = 20
+// alert(UserAge)
+
+// (Chapter Number 4)
+
+// var a = "An Apple a day keeps a doctor away "
+// var b = "Hello World!"
+// var c = "Tit For Tat"
+// alert([a,b,c])
+
+// var legal = "Variable names can only contain ___variable___, ___$___, ___1st variable then numbers___."
+// var illegal = "illegal variable special characters, numbers, etc."
+// alert([legal,illegal])
+
+// (Chapter Number 5)
+
+// var numbers1 = 5
+// var numbers2 = 3
+// var total = numbers1 + numbers2
+// document.write( "Total : " , total)
+// var numbers1 = 5
+// var numbers2 = 3
+// var total2 = numbers1 * numbers2 * 8
+// document.write( "Total : " , total2)
+// var total3 = numbers1 * numbers2 * 8 / 3
+// document.write( "Total : " , total3)
+
+// (Chapter Number 6)
+
+// var a = 10
+// a = ++a
+// var b = 16
+// b = --b
+// var c = 40
+// c = c++
+// var d = 29
+// d = c--
+
+// var Total4 = a + b / c * d
+
+// document.write(Total4)
+// var a = 60
+// a = ++a
+// var b = 16
+// b = --b
+// var c = 40
+// c = c++
+// var d = 29
+// d = c--
+
+// var Total5 = a + b + c + d
+
+// document.write(Total5)
+
+// (Chapter Number 7 and 8)
+
+// var Eng = 20
+// document.write("Eng",Eng)
+// var Math = 17
+// document.write("Math",Math)
+// var Urdu = 18
+// document.write("Urdu",Urdu)
+// var Isl = 19
+// document.write("Isl",Isl)
+// var Phy = 20
+// document.write("Phy",Phy)
+// var total6 = Eng+Math+Urdu+Isl+Phy
+// document.write("Total : ",total6)
+// var Percentage = Eng+Math+Urdu+Isl+Phy*100/100
+// document.write("Percentage",Percentage)
+
+// (Chapter Number 9 and 11)
+
+// var a = prompt("City")
+// if(a == "Karachi"){
+//     document.write("User Enter")
+// }
+// else{
+//     document.write("User Not Enter")
+// }
+// var a= prompt("Color red,green,yellow")
+// if(a == 'green'){
+//     document.write("User Allowed")
+// }
+// else{
+//     document.write("User Not Allowed")
+// }
+
+// (Chapter Number 11 and 12)
+
+// var Eng = +prompt("Eng");
+// var Math = +prompt("Math");
+// var Isl = +prompt("Isl");
+// var Urdu = +prompt("Urdu");
+// var Phy = +prompt("Phy");
+
+// var total6 = Eng + Math + Urdu + Isl + Phy
+// document.write("Total : ", total6)
+// var Percentage = total6 * 100 / 500
+// document.write("Percentage",Percentage)
+// if (Percentage > 70) {
+//     document.write("User Passed")
+// }
+// else {
+
+//     document.write("User Failed")
+// }
+// var password = "Huzaifa"
+// var pass = prompt("enter password")
+// if(password == pass){
+//     document.write("User Enter")
+// }
+// else{
+//     document.write("Pleaase Enter a Valid password")
+// }
+
+// (Chapter Number 13 and 15)
+
+// var Qualification = ["PhD","Mphil","MS","Bcom","Bsc","HSC","SSC"]
+// console.log("Qualification : ",Qualification)
+
+// var a = "The Score OF Michel is 320. Percentage  = 70%"
+// var b = "The Score OF Rich is 360. Percentage  = 80%"
+// var c = "The Score OF Bela is 260. Percentage  = 50%"
+// var Student = [a,b,c]
+// document.write(Student)
+// document.write(student2,student0,student1)
+
+// var arr = ["This ","is ","my ","cat "]
+// document.write(arr)
+
+// (Chapter Number 17 and 20)
+
+// var range = [1,2,3,4,5,6,7,8,9,10]
+// console.log(range)
+
+// var a = 2 
+// var table = prompt("Enter a number for a table",a)
+// var tab = ["2 × 1 = 2 ,	2 × 6 = 12 ,2 × 2 = 4 ,	2 × 7 = 14 ,2 × 3 = 6 ,	2 × 8 = 16 ,2 × 4 = 8 ,	2 × 9 = 18 ,2 × 5 = 10 ,2 × 10 = 20 "
+// ]
+// if(table==a){
+//   document.write(tab)
+// }
+// else{
+//     document.write("Enter a valid number")
+// }
+
+// var A = ("a. Counting: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15")
+// var B = ("b. Reverse counting: 10, 9, 8, 7, 6, 5, 4, 3, 2, 1")
+// var C = ("c. Even: 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20")
+// var D = ("d. Odd: 1, 3, 5, 7, 9, 11, 13, 15, 17, 19")
+// var abcd = A+B+C+D
+// console.log(abcd)
+
+// var A = ["cake", "apple", "pie", "cookie", "chips", "patties"]
+// var backeries = prompt("Search item")
+// for (var i = 0; i <= A.length; i++) {
+//     if(backeries == A[i]){
+//         document.write("KAKA backery")
+//         break
+//     }
+//     else{
+//         document.write("item not found")
+//     }
+
+// }
+
